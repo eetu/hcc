d2d2af642d3cead06eebbfe2664b41d4508abd46
Eetu Sutinen <eetu.sutinen@gmail.com>
2022-10-01 11:43:47 +0300

    Preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,16 +20,41 @@ class MyDocument extends Document {
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="alternate icon" href="/favicon.ico" />
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap"
+            rel="preload"
+            as="style"
+            crossOrigin="anonymous"
+          />
+          <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap"
             rel="stylesheet"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+            rel="preload"
+            as="style"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
             rel="stylesheet"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="preload"
+            as="style"
           ></link>
           <link
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
+            crossOrigin="anonymous"
           ></link>
         </Head>
         <body>

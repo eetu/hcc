19c79cec8eb800035286715b389388b464743949
Eetu Sutinen <eetu.sutinen@gmail.com>
2022-10-02 13:41:18 +0300

    Cleanup

--- components/useCurrentTime.ts
+++ components/useCurrentTime.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useCurrentTime = () => {
   const [currenTime, setCurrentTime] = useState(new Date());
